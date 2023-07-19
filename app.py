594a89c854e195b8caccc79525e64247866837c5
stosoroc <108575040+stosoroc@users.noreply.github.com>
2023-07-19 15:06:45 +0300

    Update app.py
    am scos from flask_login import LoginManager, login_user, login_required, logout_user, current_user

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import sqlite3
 import os
 from flask import Flask, render_template, request, g, flash, abort, redirect, url_for, json, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
 
 app = Flask(__name__)
