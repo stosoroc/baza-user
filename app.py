ce756d7eba1067921c133789008d91a5324cff39
stosoroc <108575040+stosoroc@users.noreply.github.com>
2023-07-22 09:22:36 +0300

    Update app.py
    newtemp1

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config.from_object(__name__)
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://seriouspostal:123123.bbb@cluster0.txedt1k.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://newtemp1:456123.qaz@cluster0.txedt1k.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
